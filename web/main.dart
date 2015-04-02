ca321e38d974fce9e0f5c50a7cbf02b07b6253b7
Kornilios Kourtis <kornilios@gmail.com>
2015-04-02 20:04:46 +0200

    match 3 mechanic

--- web/main.dart
+++ web/main.dart
@@ -2,15 +2,34 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
-import 'dart:math';
 
 class GridPoint {
     final int x, y;
     GridPoint(this.x, this.y);
 
+    GridPoint.fromMove(GridPoint p, List<int> m)
+        : x = p.x + m[0],
+          y = p.y + m[1];
+
     bool operator ==(p) {
-        return (p is GridPoint && p.x == x && p.y == y);
+        return (p is GridPoint) && (p.x == x) && (p.y == y);
     }
+
+    String toString() {
+        return "GridPont(" + x.toString() + "," + y.toString() + ")";
+    }
+
+    int get hashCode {
+        return (x + y);
+    }
+}
+
+prGridPoint(p) {
+    if (p == null)
+        print(p);
+    else if (p is GridPoint)
+        print([p.x,p.y]);
+    else assert(false);
 }
 
 
@@ -23,12 +42,27 @@ class Elem {
         return level == 0;
     }
 
+    // apparently if you want to override ==, you need to implement
+    // hashCode() :/ Moreover, equality is used by default in things like sets
+    // and maps, so you cannot distinguish between two equal (yet different)
+    // objects in a (default) map.
+    bool equal(Elem e) {
+        return (e.level == level);
+    }
+
     String toString() {
         return level.toString();
     }
 }
 
+// A match3 grid
 class Grid {
+    static const up    = const  [ 0,-1];
+    static const down  = const  [ 0, 1];
+    static const left  = const  [-1, 0];
+    static const right = const  [ 1, 0];
+    static const moves = const [up,down,left,right];
+
     final int x, y;
     List<Elem> elems;
 
@@ -40,20 +74,63 @@ class Grid {
         return elems[p.y*x + p.x];
     }
 
-    void setElem(GridPoint p, Elem e) {
-       elems[p.y*x + p.x] = e;
+    bool validPoint(GridPoint p) {
+        return (p.x >= 0 && p.x < x && p.y >= 0 && p.y < y);
+    }
+
+    Iterable<GridPoint> neighbors(GridPoint p) {
+        return moves.map((m) => new GridPoint.fromMove(p,m))
+                    .where(validPoint);
+    }
+
+    Set<GridPoint> matchingPoints(GridPoint p) {
+        Elem e = getElem(p);
+        Set<GridPoint> s = new Set.from([p]);
+        Set<GridPoint> boundary = s;
+        while (true) {
+            Set<GridPoint> s_new = new Set();
+            for (GridPoint o in boundary)
+                s_new.addAll(neighbors(o).where(
+                        (x) => getElem(x).equal(e) && !s.contains(x)
+                ));
+
+            if (s_new.length == 0)
+                break;
+
+            s.addAll(s_new);
+            boundary = s_new;
+        }
+
+        return s;
+    }
+
+    // set an element in the grid, return the updated points that need to be
+    // redrawn
+    Set<GridPoint> setPoint(GridPoint p) {
+        int level = 1;
+        Elem e = elems[p.y*x + p.x];
+        assert(e.isEmpty());
+
+        var ret = new Set();
+        while (true) {
+            e.level = level;
+
+            var matchedPs = matchingPoints(p);
+            ret.addAll(matchedPs);
+            if (matchedPs.length < 3)
+                break;
+
+            for (GridPoint mp in matchedPs)
+                elems[mp.y*x + mp.x].level = 0;
+
+            level++;
+        }
+
+       return ret;
     }
 
     void incElem(int xi, int yi, int lvl) {
        elems[yi*x + xi].level += 1;
-    }
-}
-
-void prSquare(sq) {
-    if (sq == null) {
-        print(sq);
-    } else {
-        print([sq.x, sq.y]);
     }
 }
 
@@ -66,7 +143,7 @@ class DrawnGrid {
     Grid grid;
     num xsize, ysize;
     CanvasRenderingContext2D ctx;
-    var hlSquare = null; // current highlighted square
+    GridPoint hlSquare = null; // current highlighted square
 
     num stepSizeX, stepSizeY;
     num boxDimX, boxDimY;
@@ -122,11 +199,10 @@ class DrawnGrid {
 
     void HlSquare(num x, num y) {
         var p = getGridCoords(x,y);
-        prSquare(p);
+        prGridPoint(p);
 
-        if (p == hlSquare) {
+        if (p == hlSquare)
             return;
-        }
 
         if (p != null) {
             ctx.fillStyle = hlColor;
@@ -149,27 +225,18 @@ class DrawnGrid {
         }
     }
 
-    void updateElem(GridPoint p, int lvl) {
-        grid.setElem(p, new Elem(lvl));
-        if (lvl != 0) {
-            var x = boxBorderSize + (p.x*stepSizeX) + (boxDimX/2);
-            var y = boxBorderSize + (p.y*stepSizeY) + (boxDimY/2);
-            ctx.fillStyle = "black";
-            ctx.fillText(lvl.toString(), x, y);
-            print(lvl.toString());
-        }
-    }
-
-    void addItem(num x, num y) {
-        GridPoint sq = getGridCoords(x,y);
-
-        if (sq == null)
+    void setPoint(num x, num y) {
+        GridPoint p = getGridCoords(x,y);
+        if (p == null)
             return;
 
-        if (!grid.getElem(sq).isEmpty())
+        if (!grid.getElem(p).isEmpty())
             return;
 
-        updateElem(sq, 1);
+        var updated = grid.setPoint(p);
+        for (var u in updated)
+            drawGridElem(u);
+
     }
 }
 
@@ -202,8 +269,6 @@ void main() {
     });
 
     canvas.onMouseDown.listen( (e) {
-        dg.addItem(e.client.x, e.client.y);
+        dg.setPoint(e.client.x, e.client.y);
     });
-
-    // TODO: match 3 mechanic
 }
