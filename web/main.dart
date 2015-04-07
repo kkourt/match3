62cd59923c8962fc9b913a7c3f5949334c10e32a
Kornilios Kourtis <kornilios@gmail.com>
2015-04-07 23:17:50 +0200

    licence and trivial readme

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,6 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2015, Kornilios Kourtis. All rights reserved.
+// This file is distributed under the terms of the MIT Licece (see the LICENSE
+// file).
 
 import 'dart:html';
 
